522aae102b17727250d89c16d8afe47dfafe7a16
Andrianjato Misa <andrianjato.misa.rajaona@esti.mg>
2018-10-15 14:55:41 +0200

    ajout

---
+++ src/TestJenkins.java
@@ -0,0 +1,13 @@
+
+public class TestJenkins {
+
+	public void showHello() {
+		System.out.println("Hello World");
+	}
+	
+
+	public static void main(String []args) {
+		TestJenkins jk = new TestJenkins();
+		jk.showHello();
+	}
+}
